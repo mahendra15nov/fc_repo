fc142c2f099ea0b4fe68029d317487dedb3f56d8
unknown <praneethreddysingireddy4@gmail.com>
2023-01-25 16:30:29 -0500

    adding my assignment

--- Praneeth.java
+++ Praneeth.java
@@ -37,12 +37,35 @@ public class Praneeth {
             }
         }
     }
+    public static void count_spaces(String str){
+        final int MAX_CHAR = 256;
+         int count[] = new int[MAX_CHAR];
+        int len = str.length();
+        for (int i = 0; i < len; i++){
+            count[str.charAt(i)]++;
+        }
+        char ch[] = new char[str.length()];
+        for (int i = 0; i < len; i++) {
+            ch[i] = str.charAt(i);
+            int find = 0;
+            for (int j = 0; j <= i; j++) {
+                if (str.charAt(i) == ch[j])
+                    find++;
+            }
+            if (find == 1) {
+                System.out.println(
+                        "Number of Occurrence of "+ str.charAt(i)+ " is:" + count[str.charAt(i)]);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
+        String spaces=sc.nextLine();
         int number = sc.nextInt();
         palindrome(name);
         count(number);
+        count_spaces(spaces);
     }
 }
